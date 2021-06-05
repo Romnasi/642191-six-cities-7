92caeb066bd93181267e5e8f8a9974ef52582fa2
Roman Holzman <romnasi@gmail.com>
2021-06-05 23:35:25 +0300

    Добавляет React-компоненты: Main, PlaceCard

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Main from '../main/main';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {cardData} = props;
+  return (
+    <Main cardData={cardData}/>
+  );
 }
+
+App.propTypes = {
+  cardData: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      photo: PropTypes.string.isRequired,
+      isPremium: PropTypes.bool.isRequired,
+      isBookmark: PropTypes.bool.isRequired,
+      rating: PropTypes.number.isRequired,
+    })),
+};
 
 export default App;

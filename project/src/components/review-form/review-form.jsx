94b483356778ad251ed331f0c297dd2ace2a8750
Roman Holzman <romnasi@gmail.com>
2021-06-21 00:19:09 +0300

    Реализует сохранение введённых в форму данных в state компонента

--- project/src/components/review-form/review-form.jsx
+++ project/src/components/review-form/review-form.jsx
@@ -1,47 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const grades = ['perfect', 'good', 'not bad', 'badly', 'terribly'];
+const maxRate = grades.length;
 
 
 function ReviewForm() {
+  const [ratingData, setRatingData] = useState({
+    comment: '',
+    rating: 0,
+  });
+
   return (
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
+        {grades.map((grade, i) => (
+          <>
+            <input
+              className="form__rating-input visually-hidden"
+              name="rating"
+              value={maxRate - i}
+              id={`${maxRate - i}-stars`}
+              type="radio"
+              onChange={() => {
+                setRatingData({
+                  ...ratingData,
+                  rating: maxRate - i,
+                });
+              }}
+            />
+            <label
+              htmlFor={`${maxRate - i}-stars`}
+              className="reviews__rating-label form__rating-label"
+              title={grade}
+            >
+              <svg className="form__star-image" width="37" height="33">
+                <use xlinkHref="#icon-star" />
+              </svg>
+            </label>
+          </>
+        ))}
 
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
       </div>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" />
+
+      <textarea
+        className="reviews__textarea form__textarea"
+        id="review"
+        name="review"
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        onChange={({target}) => {
+          const value = target.value;
+          setRatingData({
+            ...ratingData,
+            comment: value,
+          });
+        }}
+      />
+
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and
@@ -49,6 +65,7 @@ function ReviewForm() {
         </p>
         <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
       </div>
+
     </form>
   );
 }

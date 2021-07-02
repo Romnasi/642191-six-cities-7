e392f23a0eb9c1150f42334fda0b97e4156f290c
Roman Holzman <romnasi@gmail.com>
2021-07-02 18:04:38 +0300

    Подключает Redux

--- project/src/index.js
+++ project/src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import {offers} from './mocks/offer';
 import {reviews} from './mocks/reviews';
+import {reducer} from './store/reducer';
+import {ActionCreator} from './store/action';
+
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
+
+store.dispatch(ActionCreator.setOffers(offers));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      offers={offers}
-      reviews={reviews}
-    />
+    <Provider store={store}>
+      <App
+        reviews={reviews}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));

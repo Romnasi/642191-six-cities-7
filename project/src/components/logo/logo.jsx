aa05afbc065b1110cb3b7cc89baeaeb2d4fa9356
Roman Holzman <romnasi@gmail.com>
2021-07-21 01:34:08 +0300

    Рефакторит ссылки на лого согласно линтеру

--- project/src/components/logo/logo.jsx
+++ project/src/components/logo/logo.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import OptionalLink from '../optional-link/optional-link';
-import PropTypes from 'prop-types';
 import {AppRoute} from '../../const';
+import {Link} from 'react-router-dom';
 
 
-function Logo({isMainScreen}) {
+function Logo() {
   return (
-    <OptionalLink className={'header__logo-link header__logo-link--active'} isMainScreen={isMainScreen} to={AppRoute.ROOT}>
+    <Link className={'header__logo-link header__logo-link--active'} to={AppRoute.ROOT}>
       <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-    </ OptionalLink>
+    </ Link>
   );
 }
-Logo.propTypes = {
-  isMainScreen: PropTypes.bool,
-};
 
 
 export default Logo;
